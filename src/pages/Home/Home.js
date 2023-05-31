6d18bd12b3069506905e8d3ca166e42634c93915
malkmaneth <miguelflores.preface@gmail.com>
2023-05-31 11:42:41 +0800

    fix: layout for homepage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,15 @@
 // setup the home page
 import React from 'react';
+import Grid from '@mui/material/Grid'; // Grid version 1
+
 import Navbar from '../../components/ui/Navbar';
 import Quickmatch from '../../containers/Home/Quickmatch';
 import Friends from '../../containers/Home/Friends';
+import Carousel from '../../containers/Home/Carousel';
+import Favorites from '../../containers/Home/Favorites';
+import Rooms from '../../containers/Home/Rooms';
+import History from '../../containers/Home/History';
+
 
 function Home() {
     return (
@@ -10,14 +17,24 @@ function Home() {
             {/* navbar */}
             <Navbar />
 
-            {/* left column: Quickmatch & Friends */}
-            <Quickmatch />
-            <Friends />
+            <Grid container spacing={3}>
+                {/* left column: Quickmatch & Friends */}
+                <Grid item={true} xs>
+                    <Quickmatch />
+                    <Friends />
+                </Grid>
+                {/* middle: Carousel & Favorites */}
+                <Grid item={true} xs={6}>
+                    <Carousel />
+                    <Favorites />
+                </Grid>
+                {/* right column: Rooms & History */}
+                <Grid item={true} xs>
+                    <Rooms />
+                    <History />
+                </Grid>
+            </Grid>
 
-            {/* middle: Carousel & Favorites */}
-
-            {/* right column: Rooms & History */}
-            
         </div>
     );
 }
