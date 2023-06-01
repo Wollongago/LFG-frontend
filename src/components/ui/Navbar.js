4dc21b9bc688c3bc1f2ac9ba5017969773f7202e
malkmaneth <miguelflores.preface@gmail.com>
2023-06-01 17:22:32 +0800

    chore: add navbar outline

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import { AppBar, Button, Container, Stack } from '@mui/material'
+import Searchbar from './Searchbar'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <AppBar 
+            elevation={0}
+            sx={{
+                py: 2,
+                height: '100px'
+            }}
+        >
+            <Container>
+                <Stack 
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-evenly"
+                    flexWrap="wrap"
+                    spacing={3}
+                >
+                    <p>Logo</p>
+                    {/* Link */}
+                    <Stack
+                        direction="row"
+                        alignItems="center"
+                        jusityContent="center"
+                        spacing={3}
+                        
+                    >
+                    <p>LFG</p>
+
+                    <p>About Us</p>
+                    </Stack>
+                    
+                    {/* Search bar */}
+                    <Stack 
+                        alignItems="center"
+                        sx={{ flex:1 }}    
+                        flex="wrap"
+                    >
+                        <Searchbar />
+                    </Stack>
+                    
+                    <Stack 
+                        direction="row"
+                        alignItems="center"
+                        jusityContent="center"
+                        spacing={3}
+                    >
+                        {/* Sign up */}
+                        <p>Sign Up</p>
+                        {/* Log in */}
+                        <p>Log in</p>
+                    </Stack>
+                    
+
+                </Stack>
+            </Container>
+        </AppBar>
+
     )
 }
 
